825241bf137e944d7a557a9e00b769dadff33b0f
P@piH@ck3R <mssmbaye@gmail.com>
2018-01-01 00:36:43 +0000

    Database configuration

--- itdev/prod_settings.py
+++ itdev/prod_settings.py
@@ -1,7 +1,10 @@
 from .settings import *
+import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = 'e+@i0%kr6ku^o&&$#t=yi0$$s=(4spi9(hak46*fwjqv*jc0+v'
 
