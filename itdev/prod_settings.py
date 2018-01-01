8f7670ed957406c1f6f9b3ad90f4ff3103d81ed8
P@piH@ck3R <mssmbaye@gmail.com>
2018-01-01 01:33:20 +0000

    setup static files

--- itdev/prod_settings.py
+++ itdev/prod_settings.py
@@ -6,9 +6,9 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
-MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
