94bf2a9c099c9529bb66fa13bfa796c03ea9e415
P@piH@ck3R <mssmbaye@gmail.com>
2018-01-01 01:37:23 +0000

    setup static files

--- itdev/views.py
+++ itdev/views.py
@@ -6,4 +6,4 @@ def handler404(request):
     return render(request, 'errors/404.html', status=404)
 
 def handler500(request):
-    return render(requesy, 'errors/500.html', status=500)
+    return render(request, 'errors/500.html', status=500)
