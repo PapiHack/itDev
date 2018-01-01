e0e9cc2ebe2464279d0d696b70ffdcbc6056b390
P@piH@ck3R <mssmbaye@gmail.com>
2018-01-01 03:13:52 +0000

    first

--- itdev/prod_settings.py
+++ itdev/prod_settings.py
@@ -6,8 +6,8 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
-#MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 SECRET_KEY = os.environ['SECRET_KEY']
