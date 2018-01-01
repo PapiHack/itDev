9a5b70e4725cf4107a4d9088e6a220394ec90ec7
P@piH@ck3R <mssmbaye@gmail.com>
2018-01-01 01:11:09 +0000

    First Commit

--- itdev/prod_settings.py
+++ itdev/prod_settings.py
@@ -6,7 +6,9 @@ TEMPLATE_DEBUG = False
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECRET_KEY = 'e+@i0%kr6ku^o&&$#t=yi0$$s=(4spi9(hak46*fwjqv*jc0+v'
 
